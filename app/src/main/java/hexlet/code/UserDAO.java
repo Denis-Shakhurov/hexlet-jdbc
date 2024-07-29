841e041f855f6177a7d87833b27d247aa75d96a8
DenisShakhurov <disa.sdf86@gmail.com>
2024-07-29 09:54:45 +0700

    create User.java UserDAO.java

---
+++ app/src/main/java/hexlet/code/UserDAO.java
@@ -0,0 +1,67 @@
+package hexlet.code;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+
+public class UserDAO {
+    private Connection connection;
+
+    public UserDAO(Connection conn) {
+        this.connection = conn;
+    }
+
+    public void save(User user) throws SQLException {
+        if (user.getId() == null) {
+            var sql = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, user.getUsername());
+                preparedStatement.setString(2, user.getPhone());
+                preparedStatement.executeUpdate();
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    user.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+            }
+        } else {
+            var sql = "UPDATE users SET ? = ?";
+            try (var preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, "username");
+                preparedStatement.setString(2, user.getUsername());
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "phone");
+                preparedStatement.setString(2, user.getPhone());
+                preparedStatement.executeUpdate();
+            }
+        }
+    }
+
+    public Optional<User> find(Long id) throws SQLException {
+        var sql = "SELECT * FROM users WHERE id = ?";
+        try (var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            var resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                var userName = resultSet.getString("username");
+                var phone = resultSet.getString("phone");
+                User user = new User(userName, phone);
+                user.setId(id);
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        }
+    }
+
+    public void remove(User user) throws SQLException {
+        var sql5 = "DELETE FROM users WHERE username = ?";
+
+        try (var preparedStatement = connection.prepareStatement(sql5)) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.executeUpdate();
+        }
+    }
+}
